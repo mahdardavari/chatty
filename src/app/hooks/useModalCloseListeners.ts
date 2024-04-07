4065435a8b208a187be1681711363ee9007823aa
Mahdar <mahdardavari@gmail.com>
2024-04-07 16:11:02 +0330

    Close modal on browser back button click

--- src/app/hooks/useModalCloseListeners.ts
+++ src/app/hooks/useModalCloseListeners.ts
@@ -1,16 +1,16 @@
 import { useEffect, useRef } from 'react';
 
-export const useModalCloseListeners = (handleClose :() => void) => {
+export const useModalCloseListeners = (handleClose: () => void) => {
   const modalRef = useRef(null);
 
   useEffect(() => {
-    const handleKeyDown = (event :KeyboardEvent) => {
+    const handleKeyDown = (event: KeyboardEvent) => {
       if (event.key === "Escape") {
         handleClose(); // Close modal on ESC key press
       }
     };
 
-    const handleClickOutside = (event :MouseEvent) => {
+    const handleClickOutside = (event: MouseEvent) => {
       if (
         modalRef.current &&
         !(modalRef.current as HTMLDivElement).contains(event.target as Node)
@@ -19,12 +19,18 @@ export const useModalCloseListeners = (handleClose :() => void) => {
       }
     };
 
+    const handlePopstate = () => {
+      handleClose(); // Close modal on browser back button click
+    };
+
     window.addEventListener("keydown", handleKeyDown);
     document.addEventListener("mousedown", handleClickOutside);
+    window.addEventListener("popstate", handlePopstate)
 
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
       document.removeEventListener("mousedown", handleClickOutside);
+      window.removeEventListener("popstate", handlePopstate);
     };
   }, [handleClose, modalRef]);
 
